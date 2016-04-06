22733cd95bf057e3179c5b13483650926bb62882
Gray Reinhard <gray.reinhard@gmail.com>
2016-04-06 13:15:28 -0400

    update search form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_subsQuery
+  private
+  def set_subsQuery
+  	@subsQuery = Sub.all.pluck_h(:name, :adsh)
+  end
 end
