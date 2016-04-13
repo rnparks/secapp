55f291e2bf3d286dcc6a52288e4d623021857f06
Gray Reinhard <gray.reinhard@gmail.com>
2016-04-13 12:17:40 -0400

    add an import_data:all task and create xbrl view

--- app/controllers/xbrls_controller.rb
+++ app/controllers/xbrls_controller.rb
@@ -1,2 +1,12 @@
 class XbrlsController < ApplicationController
+	before_action :set_sub
+
+	def index
+  end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_sub
+		@sub = Sub.find(params[:sub_id])
+	end
 end
