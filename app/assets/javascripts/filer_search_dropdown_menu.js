34ea05713aac648e1f73bb25aef3cbc2f87f3575
Gray Reinhard <gray.reinhard@gmail.com>
2016-04-21 15:49:36 -0400

    add turbolinks workaround for document ready

--- app/assets/javascripts/filer_search_dropdown_menu.js
+++ app/assets/javascripts/filer_search_dropdown_menu.js
@@ -1,4 +1,5 @@
-$( document ).ready(function() {
+var ready;
+ready = function() {
 
 	var filerList = new Bloodhound({
 		datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.searchName); },
@@ -45,4 +46,6 @@ $( document ).ready(function() {
 		return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 	}
 
-});
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
