978f5bfcab9596f4b474c72f266962907faa31b4
Gray Reinhard <gray.reinhard@gmail.com>
2016-04-22 13:35:47 -0400

    make nums link to xbrl

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,6 +1,7 @@
 class Sub < ActiveRecord::Base
 	self.primary_key = :adsh
 	has_one :stock, :foreign_key => 'cik', :primary_key => 'cik'
+	has_one :filer, :foreign_key => 'cik', :primary_key => 'cik'
 	has_many :nums, :foreign_key => 'adsh', :primary_key => 'adsh'
 	has_many :xbrls, :foreign_key => 'cik', :primary_key => 'cik'
 	has_many :sics, :foreign_key => 'sic', :primary_key => 'sic'
